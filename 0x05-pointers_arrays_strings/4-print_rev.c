cccdf0df965dcf5437bb724ce5e8bceafcbc247d
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-02-27 13:42:31 -0800

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_rev - print string in reverse order
+* @s: string
+* Return: Void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0, n = 0;
+	
+	while (s[n] != '\0')
+		n++;
+
+	for (i = n - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
