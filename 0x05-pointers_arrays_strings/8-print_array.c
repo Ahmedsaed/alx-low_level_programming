0eb6df7dc770c646144aa49215a1709c1c5aa303
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-02-27 14:31:49 -0800

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+* print_array - print array elements
+*
+* @a: array pointer
+* @n: array size
+* Return: Void
+*/
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(a[i]);
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+	_putchar('\n');
+}
