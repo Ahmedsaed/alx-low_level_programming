125de45e589cb60867bd08a238bb251b7a8a054c
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-02-27 13:16:00 -0800

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,11 @@
 #include"main.h"
 
-/*
- * reset_to_98 - sets the value to 98
- *
- * @n: int to change
- *
- * Return: Void
- */
-
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: int to change
+*  Owned By ahmedsaed
+*  Return: 0 is success
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
