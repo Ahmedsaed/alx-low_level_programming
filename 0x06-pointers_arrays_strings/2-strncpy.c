1160b1c63d91eee48b4cc3e704358cff6b8ece19
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-02 14:16:14 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* _strncpy - copy n bytes from source string to destination string
+*
+* @dest: destination string
+* @src: source string
+* @n: bytes to copy
+*
+* Return: A pointer to the resulting string
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	char *tmp = dest;
+
+	for (i = 0; i < n; i++)
+	{
+		if (*src == '\0')
+			break;
+
+		*dest = *src;
+		src++;
+		dest++;
+	}
+
+	*dest = '\0';
+
+	return (tmp);
+}
