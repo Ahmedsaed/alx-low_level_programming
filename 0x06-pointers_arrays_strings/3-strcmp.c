9d642c8ebf6cd0f357a8562214e22bd46796857d
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-02 14:39:13 -0800

    strncat

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,14 @@ int _strcmp(char *s1, char *s2)
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		if (*s1 - *s2 != 0)
+		{
 			return (*s1 - *s2);
+		}
+		else
+		{
+			src++;
+			dest++;
+		}
 	}
 
 	return (0);
