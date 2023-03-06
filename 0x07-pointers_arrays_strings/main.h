c289bf0a874abcbf05f192a1c75f635c4f27a8ba
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-06 14:46:32 -0800

    print_diagrams

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stddef.h>
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_strchr(char *s, char c);
+void print_diagsums(int *a, int size);
 
 #endif
