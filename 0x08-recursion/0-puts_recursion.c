f8b4ad2a9430c71d36ad35c4e3f21232341c42f2
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-08 12:51:47 -0800

    put recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: pointer to a string
+ *
+ * Return: Void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_putchar(*s);
+	_puts_recursion(++s);
+}
