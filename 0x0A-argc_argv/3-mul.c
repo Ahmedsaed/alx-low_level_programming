98bc3e33ac3f42a0e59f2788a63092131007730e
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-03-10 14:45:55 -0800

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies two numbers
@@ -11,13 +12,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	printf("%d\n", (*argv[1] - '0') * (*argv[2] - '0'));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return 0;
 }
