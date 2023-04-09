c1d3b90d51fc55ed8e575c38ddc234425b0b64de
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-04-08 18:33:52 -0700

    bug fixes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[n] != '\0')
+	while (text_content != NULL && text_content[n] != '\0')
 		n++;
 
 	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
